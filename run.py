6716c38dc0e068ad6646f6819bf00ae202366ab3
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-03-29 20:21:15 +0300

    implement auth basics

--- run.py
+++ run.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_httpauth import HTTPTokenAuth
+from db import settings
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
 
-from db import settings
+######### Authentication ###########
+auth = HTTPTokenAuth(scheme='Bearer')
+
+# Register authentication callback
+@auth.verify_token
+def verify_token(token):
+  DEV_TOKEN = 'TOKEN'
+  return token == DEV_TOKEN
+  # TODO Add some real authentication logic.
+
+# Register error callback
+@auth.error_handler
+def auth_error(status):
+    return jsonify({'message': 'gtfo'}), status
+
+# Authentication example for route
+@app.route('/auth/test')
+@auth.login_required # <-- Auth decorator for secret routes
+def auth():
+  return jsonify({'message': 'You may enter'})
 
 @app.route("/")
 def index():
   return "index"
+
