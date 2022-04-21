a6ac6e815f6577ab994e16e8ce4b2ce8f9e9016c
Paattis <leevi.paattilammi@gmail.com>
2022-04-21 12:42:25 +0300

    * refactor app.py for waitress-server * add waitress to requirements for deployment convenience

--- app.py
+++ app.py
@@ -8,95 +8,72 @@ import spotipy
 import os
 import json
 from dotenv import load_dotenv
-load_dotenv()
 
-app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET_KEY')
-# TODO: Better configs
+def create_app(test_config=None):
+  load_dotenv()
 
-# add modules to path for easy access
-module_path = [
-  'db',
-]
+  app = Flask(__name__)
+  app.secret_key = os.getenv('FLASK_SECRET_KEY')
+  # TODO: Better configs
 
-for path in module_path:
-  sys.path.append(path)
+  # add modules to path for easy access
+  module_path = [
+    'db',
+  ]
 
-
-# Setup SQLAlchemy
-database, migrate = db.init_app(app)
-
-@app.route('/location/<name>', methods=['POST',])
-@app.errorhandler(500)
-def add_location(name: str):
-  """Adds a location to the database and returns the id.
-  If the location already exists, return it instead
-
-  Args:
-      name (str): The name of the location
-
-  Returns:
-      dict: the representation of the location object
-  """
-  import re
-
-  # sanitize the string a bit, remove all non-alphanumeric
-  # characters from the beginning and the end of the string
-  # and also turn it into title case (e.g. 'helsinki-vAnTaa' -> 'Helsinki-Vantaa')
-  name = re.sub(r'(^\W+|\W+$)', '', name.title())
-
-  location = Location.query.filter(
-    Location.name==name
-  ).first()
-
-  if not location:
-    # if the location wasn't found, create a new one
-    try:
-      location = Location(name=name)
-      db.db.session.add(location)
-      db.db.session.commit()
-    except Exception as e:
-      return {'msg': 'Something went wrong when saving location to the database'}, 500
-
-  return location.to_dict()
+  for path in module_path:
+    sys.path.append(path)
 
 
-@app.route('/location/<location_id>/')
-@app.errorhandler(404)
-def get_location(location_id: int):
-  """Returns the location with the corresponding id
+  # Setup SQLAlchemy
+  database, migrate = db.init_app(app)
 
-  Args:
-      location_id (int): the id of the location
+  @app.route('/location/<name>', methods=['POST',])
+  @app.errorhandler(500)
+  def add_location(name: str):
+    """Adds a location to the database and returns the id.
+    If the location already exists, return it instead
 
-  Returns:
-      dict: The corresponding location
-  """
-  location = Location.query.filter(
-    Location.id==location_id
-  ).first()
-
-  if not location:
-    return {'msg': 'Location not found'}, 404
-
-
-  return {'songs': location.to_dict()}
-
-
-@app.route('/location/<location_id>/songs/<spotify_id>', methods=['POST',])
-@app.errorhandler(500)
-@app.errorhandler(404)
-@app.errorhandler(400)
-def add_location_song(location_id: int, spotify_id: str):
-    """Adds a song to a location after validating that both
-    the location and the song exist.
-    
     Args:
-        location_id (int): the id of the location in the database
-        spotify_id (str): the id of the song in the spotify API
-    """
+        name (str): The name of the location
 
-    # check if the location exists in the database
+    Returns:
+        dict: the representation of the location object
+    """
+    import re
+
+    # sanitize the string a bit, remove all non-alphanumeric
+    # characters from the beginning and the end of the string
+    # and also turn it into title case (e.g. 'helsinki-vAnTaa' -> 'Helsinki-Vantaa')
+    name = re.sub(r'(^\W+|\W+$)', '', name.title())
+
+    location = Location.query.filter(
+      Location.name==name
+    ).first()
+
+    if not location:
+      # if the location wasn't found, create a new one
+      try:
+        location = Location(name=name)
+        db.db.session.add(location)
+        db.db.session.commit()
+      except Exception as e:
+        return {'msg': 'Something went wrong when saving location to the database'}, 500
+
+    return location.to_dict()
+
+
+  @app.route('/location/<location_id>/')
+  @app.errorhandler(404)
+  def get_location(location_id: int):
+    """Returns the location with the corresponding id
+
+    Args:
+        location_id (int): the id of the location
+
+    Returns:
+        dict: The corresponding location
+    """
     location = Location.query.filter(
       Location.id==location_id
     ).first()
@@ -104,66 +81,92 @@ def add_location_song(location_id: int, spotify_id: str):
     if not location:
       return {'msg': 'Location not found'}, 404
 
-    # check if the song exists
-    song = Song.query.filter(
-      Song.spotifyId==spotify_id
-    ).first()
 
-    if not song:
-      # song doesn't exist, fetch it from the Spotify API and add
-      # it to the db
-      auth_manager = DatabaseTokenCacheHandler()
-      sp = spotipy.Spotify(auth_manager=auth_manager)
+    return {'songs': location.to_dict()}
+
+
+  @app.route('/location/<location_id>/songs/<spotify_id>', methods=['POST',])
+  @app.errorhandler(500)
+  @app.errorhandler(404)
+  @app.errorhandler(400)
+  def add_location_song(location_id: int, spotify_id: str):
+      """Adds a song to a location after validating that both
+      the location and the song exist.
       
+      Args:
+          location_id (int): the id of the location in the database
+          spotify_id (str): the id of the song in the spotify API
+      """
 
-      from spotipy.exceptions import SpotifyException
-      try:
-        song_data = sp.track(spotify_id, market='FI')
-      except SpotifyException:
-        # song doesn't exist
-        return {'msg': 'Invalid song id'}, 500
+      # check if the location exists in the database
+      location = Location.query.filter(
+        Location.id==location_id
+      ).first()
+
+      if not location:
+        return {'msg': 'Location not found'}, 404
+
+      # check if the song exists
+      song = Song.query.filter(
+        Song.spotifyId==spotify_id
+      ).first()
+
+      if not song:
+        # song doesn't exist, fetch it from the Spotify API and add
+        # it to the db
+        auth_manager = DatabaseTokenCacheHandler()
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+        
+
+        from spotipy.exceptions import SpotifyException
+        try:
+          song_data = sp.track(spotify_id, market='FI')
+        except SpotifyException:
+          # song doesn't exist
+          return {'msg': 'Invalid song id'}, 500
+
+        try:
+          # add song to database if it exists
+          song = Song(
+            name=song_data.get('name'),
+            spotifyId=spotify_id,
+            # TODO: add popularity score
+          )
+          db.db.session.add(song)
+          db.db.session.commit()
+        except Exception as e:
+          return {'msg': 'Something went wrong when adding a song to the location'}, 500
+
+
+      if song in location.songs:
+        return {'msg': 'Song already in location\'s songs'}, 400
 
       try:
-        # add song to database if it exists
-        song = Song(
-          name=song_data.get('name'),
-          spotifyId=spotify_id,
-          # TODO: add popularity score
-        )
-        db.db.session.add(song)
+        location.songs.append(song)
         db.db.session.commit()
       except Exception as e:
         return {'msg': 'Something went wrong when adding a song to the location'}, 500
 
-
-    if song in location.songs:
-      return {'msg': 'Song already in location\'s songs'}, 400
-
-    try:
-      location.songs.append(song)
-      db.db.session.commit()
-    except Exception as e:
-      return {'msg': 'Something went wrong when adding a song to the location'}, 500
-
-    return {'msg': 'Song added successfully'}
+      return {'msg': 'Song added successfully'}
 
 
 
-@app.route('/songs/search/<searchTerm>')
-def search_song(searchTerm: str):
-    """Searches for songs with the search term from the Spotify API and returns them
-    
-    Args:
-        searchTerm (str): the search term
-    
-    Returns:
-        dict: the search results 
-    """
+  @app.route('/songs/search/<searchTerm>')
+  def search_song(searchTerm: str):
+      """Searches for songs with the search term from the Spotify API and returns them
+      
+      Args:
+          searchTerm (str): the search term
+      
+      Returns:
+          dict: the search results 
+      """
 
-    auth_manager = DatabaseTokenCacheHandler()
+      auth_manager = DatabaseTokenCacheHandler()
 
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    data = sp.search(q=searchTerm, limit=50, type=['track'],market='FI')
+      sp = spotipy.Spotify(auth_manager=auth_manager)
+      data = sp.search(q=searchTerm, limit=50, type=['track'],market='FI')
 
-    return data.get('tracks')
+      return data.get('tracks')
 
+  return app
